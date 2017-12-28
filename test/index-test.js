a855ce02d703a6a23a63c7f7a0b45f6184730df7
MAHAK RAJVANSHI <mahakrajvanshi@yahoo.com>
2017-12-28 04:26:51 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -18,7 +18,7 @@ describe('Objects', function() {
     it('it does not modify the original object, but rather returns a clone with the new data', function() {
       var obj = { prop: 1 }
 
-      updateObjectWithKeyAndValue(obj, 'prop2', 2)
+      updateObjectWithKeyAndValue1(obj, 'prop2', 2)
 
       expect(obj['prop2']).toBe(undefined)
     })
