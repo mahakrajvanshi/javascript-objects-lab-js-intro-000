367d32f5c95ac418bee1817c8dbc430f5bf9a701
MAHAK RAJVANSHI <mahakrajvanshi@yahoo.com>
2017-12-28 04:32:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var recipes = {'sample1': 1, 'sample2':2}
+var recipes = {}
 
-function updateObjectWithKeyAndValue(object, key, value){
-    object[key] = value
-    return object
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
 }
 
-function updateObjectWithKeyAndValue1(object, key, value){
-    var newObj = Object.assign({}, obj)
-    newObj[key] = value
-    return newObj
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+
+  return object
 }
